81a2064f19b8182feb61ab874f2ef27c8877a8ac
Salva Manzanera <salva.miguel.manzanera@gmail.com>
2024-12-04 08:13:34 +0100

    Updating user data

--- infrastructure/default/userData.sh
+++ infrastructure/default/userData.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 yum install -y httpd
 echo "<h1>Hello from $(hostname)</h1>" > /var/www/html/index.html
 systemctl start httpd
